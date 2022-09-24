76df01b6caeb8d28e181e667af91eab495c7a927
Sam Carey <sam@scarey.me>
2022-09-24 02:25:23 -0400

    Add grpc server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+use tonic::{transport::Server, Request, Response, Status};
+
+use buzzer::buzzer_server::{Buzzer, BuzzerServer};
+use buzzer::{OpenDoorRequest, OpenDoorReply, RingDoorRequest, RingDoorReply};
+use tonic::codec::CompressionEncoding;
+
+pub mod buzzer {
+    tonic::include_proto!("buzzer");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "0.0.0.0:50000".parse()?;
+    let buzzer = MyBuzzer::default();
+
+    Server::builder()
+        .add_service(BuzzerServer::new(buzzer)
+            .send_compressed(CompressionEncoding::Gzip)
+            .accept_compressed(CompressionEncoding::Gzip))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
+
+#[derive(Debug, Default)]
+pub struct MyBuzzer {}
+
+#[tonic::async_trait]
+impl Buzzer for MyBuzzer {
+    async fn ring_door(&self, request: Request<RingDoorRequest>) -> Result<Response<RingDoorReply>, Status> {
+        println!("Got a RingDoorRequest: {:?}", request);
+
+        let reply = buzzer::RingDoorReply {
+            message: "hello".into(),
+        };
+
+        Ok(Response::new(reply))
+    }
+    
+    async fn open_door(&self, request: Request<OpenDoorRequest>) -> Result<Response<OpenDoorReply>, Status> {
+        println!("Got a OpenDoorRequest: {:?}", request);
+
+        let reply = buzzer::OpenDoorReply {
+            message: "hello".into(),
+        };
+
+        Ok(Response::new(reply))
+    }
 }
