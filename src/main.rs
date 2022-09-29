f31eb8ba6444d55a3467f84f1a392d9c59cc58d5
Sam Carey <sam@scarey.me>
2022-09-29 00:18:45 -0400

    Implement server streaming for ListenDoor RPC

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use tonic::{transport::Server, Request, Response, Status};
 
 use buzzer::buzzer_server::{Buzzer, BuzzerServer};
-use buzzer::{OpenDoorRequest, OpenDoorReply, RingDoorRequest, RingDoorReply};
+use buzzer::{OpenDoorRequest, OpenDoorReply, ListenDoorRequest, RingDoorReply};
+use futures::Stream;
+use std::{time::Duration, pin::Pin, sync::{Arc, Mutex}};
+use tokio::sync::mpsc;
+use tokio_stream::{wrappers::ReceiverStream, StreamExt};
 use tonic::codec::CompressionEncoding;
+
+type RingDoorResult<T> = Result<Response<T>, Status>;
+type ResponseStream = Pin<Box<dyn Stream<Item = Result<RingDoorReply, Status>> + Send>>;
 
 pub mod buzzer {
     tonic::include_proto!("buzzer");
@@ -24,25 +31,55 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 #[derive(Debug, Default)]
-pub struct MyBuzzer {}
+pub struct MyBuzzer {
+    counter: Arc<Mutex<i64>>,
+}
 
 #[tonic::async_trait]
 impl Buzzer for MyBuzzer {
-    async fn ring_door(&self, request: Request<RingDoorRequest>) -> Result<Response<RingDoorReply>, Status> {
-        println!("Got a RingDoorRequest: {:?}", request);
 
-        let reply = buzzer::RingDoorReply {
+    type ListenDoorStream = ResponseStream; 
+
+    async fn listen_door(&self, request: Request<ListenDoorRequest>) -> RingDoorResult<Self::ListenDoorStream> {
+        println!("Got a ListenDoorRequest: {:?}", request);
+
+        // Create stream
+        let repeat = std::iter::repeat(buzzer::RingDoorReply {
             message: "hello".into(),
-        };
+        });
 
-        Ok(Response::new(reply))
+        let mut stream = Box::pin(tokio_stream::iter(repeat).throttle(Duration::from_millis(200)));
+
+        let (tx, rx) = mpsc::channel(128);
+        tokio::spawn(async move {
+            while let Some(item) = stream.next().await {
+                match tx.send(Result::<_, Status>::Ok(item)).await {
+                    Ok(_) => {
+                        // item (server response) was queued to be send to client
+                    }
+                    Err(_item) => {
+                        // output_stream was build from rx and both are dropped
+                        break;
+                    }
+                }
+            }
+            println!("\tclient disconnected");
+        });
+
+        let output_stream = ReceiverStream::new(rx);
+        Ok(Response::new(
+            Box::pin(output_stream) as Self::ListenDoorStream
+        ))
     }
     
     async fn open_door(&self, request: Request<OpenDoorRequest>) -> Result<Response<OpenDoorReply>, Status> {
         println!("Got a OpenDoorRequest: {:?}", request);
 
+        let mut counter = self.counter.lock().unwrap();
+        *counter += 1;
+
         let reply = buzzer::OpenDoorReply {
-            message: "hello".into(),
+            message: format!("{}", *counter).into(),
         };
 
         Ok(Response::new(reply))
